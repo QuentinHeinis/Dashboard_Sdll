a9e8f3901e750e788f8ef8457417b765f56ced6b
yakapi <realitygameserver@gmail.com>
2023-02-09 09:58:01 +0100

    ajout livraison PB

--- src/pages/DashBoard/LivraisonPb.jsx
+++ src/pages/DashBoard/LivraisonPb.jsx
@@ -1,46 +1,74 @@
 import React, {useEffect, useState} from 'react'
 import { createWorker } from 'tesseract.js';
 import Tesseract from 'tesseract.js';
-
-import image from '../../images/full.png'
+import { DocumentDuplicateIcon } from '@heroicons/react/outline';
 
 
 const LivraisonPb = () => {
-    const [ocr, setOcr] = useState('Recognizing...');
-
-    const worker = createWorker({
-      });
-      let quantity = []
-      let ean = []
+    const [ocr, setOcr] = useState('');
       let flip = true
-      const convert = ( ) =>{
-        ocr.split('').forEach((char)=>{
-            if(flip){
-                quantity.push(char)
-                if(char==' '){
-                    
+      let temp = ''
+      const copie = ( ) =>{
+          let quantity = []
+          let ean = []
+          let texteCopie = ''
+        ocr.split('').map((char)=>{
+            if(char === ' ' || char === `\n`){
+                console.log(temp)
+                if(flip){
+                    quantity.push(temp)
+                }else{
+                    ean.push(temp)
                 }
+                temp = ''
+                
+                flip = !flip
+            }else{
+                temp += char
             }
         })
+        for (let i = 0; i < quantity.length; i++) {
+            texteCopie += ean[i] + '\n' + 'Qte \n' + quantity[i] + '\n'
+            // console.log(ean[i] + '\n' + 'Qte \n' + quantity[i] + '\n')
+        }
+        console.log(texteCopie)
+        navigator.clipboard.writeText(texteCopie)
+        alert(texteCopie)
       }
-    useEffect(() => {
-        Tesseract.recognize(
-            image,
-            'eng',
-            {  }
-          ).then(({ data: { text } }) => {
-            setOcr(text)
-            convert()
-          })
+const getOcr = (image) =>{
+    Tesseract.recognize(
+        image,
+        'eng',
+        {  }
+      ).then(({ data: { text } }) => {
+        setOcr(text)
+      })
+}
 
-      });
+useEffect(()=>{
+let imageForm = document.getElementById('imageForm')
+imageForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    setOcr('Recognizing...')
+    let image = URL.createObjectURL(e.target[0].files[0]);
+    getOcr(image)
+})
+}, [])
 
 return(
-    <div className="App">
-      {ocr.split('').map((char)=>
-           (<p>{char}</p>)
-              )}
+<>
+<form id='imageForm' className='flex flex-col items-center border-2 border-main-rose min-w-fit w-1/3 mx-auto py-4 px-2 rounded-md mt-4'>
+    <input id='imageInput' type="file" accept="image/png, image/jpeg" required/>
+    <button>Lancer scan</button>
+</form>
+<div className='border-2 border-main-rose rounded-md w-4/5 md:w-1/2 px-2 py-4 mt-3 mx-auto flex flex-col gap-2' >
+    <div className='flex w-full items-center justify-evenly'>
+        <h1>Invent01</h1>
+        <DocumentDuplicateIcon onClick={copie}className="hover:cursor-pointer h-6"/>
     </div>
+    <div>{ocr.split('\n').map(item=>(<p>{item}</p>))}</div>
+</div>
+</>
 )
 }
 
